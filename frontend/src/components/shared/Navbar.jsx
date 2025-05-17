6fc6f18a37a47048fefd4d92fe5ad64bd6c43032
Ansh Katiyar <108641849+Royalkatiyar40@users.noreply.github.com>
2025-05-17 09:49:22 +0530

    Update Navbar.jsx

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
     const logoutHandler = async () => {
         try {
-            const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+            const res = await axios.get(`https://job-portal-848t.onrender.com/api/v1/logout`, { withCredentials: true });
             if (res.data.success) {
                 dispatch(setUser(null));
                 navigate("/");
