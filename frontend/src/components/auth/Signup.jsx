b621e00323a071c54b800b75013df92be03d7488
Ansh Katiyar <108641849+Royalkatiyar40@users.noreply.github.com>
2025-05-17 09:38:44 +0530

    Update Signup.jsx

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -56,7 +56,7 @@ const Signup = () => {
 
         try {
             dispatch(setLoading(true));
-            const res = await axios.post(https://job-portal-848t.onrender.com/api/v1/user/register", formData, {
+            const res = await axios.post("https://job-portal-848t.onrender.com/api/v1/user/register", formData, {
                 headers: { 'Content-Type': "multipart/form-data" },
                 withCredentials: true,
             });
