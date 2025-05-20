cd3794d1e85b67acf896648fd470f928c8d4c884
Ansh Katiyar <108641849+Royalkatiyar40@users.noreply.github.com>
2025-05-20 09:01:27 +0530

    Update CompanyCreate.jsx

--- frontend/src/components/admin/CompanyCreate.jsx
+++ frontend/src/components/admin/CompanyCreate.jsx
@@ -16,7 +16,7 @@ const CompanyCreate = () => {
     const dispatch = useDispatch();
     const registerNewCompany = async () => {
         try {
-            const res = await axios.post(`https://job-portal-848t.onrender.com/api/v1/register`, {companyName}, {
+            const res = await axios.post("https://job-portal-848t.onrender.com/api/v1/register", {companyName}, {
                 headers:{
                     'Content-Type':'application/json'
                 },
