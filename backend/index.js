eb1e727be9b7cef6adf0a7f426b872b06497a836
Ansh Katiyar <108641849+Royalkatiyar40@users.noreply.github.com>
2025-05-20 08:30:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-848t.onrender.com/',
+    origin:'https://job-portal-848t.onrender.com',
     credentials:true
 }
 
